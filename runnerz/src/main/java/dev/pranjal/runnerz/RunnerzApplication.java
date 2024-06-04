4c755a8e88a40da9751d6ec6783e8ce90a7bb3d3
Pranjal Saxena <pranjalsaxena5@gmail.com>
2024-06-04 09:10:55 +0530

    Learned logging

--- runnerz/src/main/java/dev/pranjal/runnerz/RunnerzApplication.java
+++ runnerz/src/main/java/dev/pranjal/runnerz/RunnerzApplication.java
@@ -1,17 +1,20 @@
 package dev.pranjal.runnerz;
 
 import foo.bar.WelcomeMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RunnerzApplication {
+	private static final Logger log = LoggerFactory.getLogger(RunnerzApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunnerzApplication.class, args);
-
-
 		var welcomeMessage = new WelcomeMessage();
 		System.out.println(welcomeMessage.getWelcomeMessage());
+		log.info("Application started Successfully!");
+		log.info("Something changed");
 	}
 }
